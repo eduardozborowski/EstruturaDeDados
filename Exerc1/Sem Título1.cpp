7e8ddb95df3ca10f9f58453690667ce0ad02ada1
Eduardo Zborowski <46855030+eduardozborowski@users.noreply.github.com>
2022-04-11 02:36:00 -0300

    Add files via upload

---
+++ Exerc1/Sem Título1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	printf("Hello world");
+	return 1;	
+}
